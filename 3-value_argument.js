e3e291ce8a989c4b830547c6293695939a466794
Bekaensarmu <beekanensarmu@gmail.com>
2025-05-23 14:30:42 +0300

    Add script to print first argument or message if none

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,7 +1,7 @@
 const firstArg = process.argv[2];
 
 if (firstArg === undefined) {
-    console.log("No argument");
-} else if (firstArg === "0") {
-    console.log("Argument found");
+  console.log("No argument");
+} else {
+  console.log(firstArg);
 }
