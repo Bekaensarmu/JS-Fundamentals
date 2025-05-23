aad29aba7236470b750b75eab158f6f164f9770c
Bekaensarmu <beekanensarmu@gmail.com>
2025-05-23 15:53:39 +0300

    Add script to print 'C is fun' x times using loop

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,15 +1,17 @@
 const x = Number(process.argv[2]);
 
-if (!Number.isInteger(x) || x < 0) {
-  console.log("Missing number of occurrences"); 
+if (!Number.isInteger(x)) {
+  console.log("Missing number of occurrences");
 } else {
-    let result = "";
-    for (let i = 0; i < x; i++) {
-        result += "C is fun\n";
-    }
-
+  let result = "";
+  for (let i = 0; i < x; i++) {
+    result += "C is fun\n";
+  }
+  if (x > 0) {
     console.log(result.trim());
+  }
 }
+
 
 
 
